bebd1646afb7735565b59ba89a651a962033f727
ChaitanyaPTank <tankchaitanya333@gmail.com>
2024-11-30 10:24:29 +0530

    feat:removed old files

--- private_dot_config/nvim/lua/plugins/lsp-zero.lua
+++ private_dot_config/nvim/lua/plugins/lsp-zero.lua
@@ -137,12 +137,12 @@ return {
       function(server_name) -- default handler (optional)
         require("lspconfig")[server_name].setup {}
       end,
+
       -- Next, you can provide a dedicated handler for specific servers.
       -- For example, a handler override for the `rust_analyzer`:
       -- ["rust_analyzer"] = function()
       --   require("rust-tools").setup {}
       -- end
-
 
       ['jsonls'] = function()
         require('lspconfig').jsonls.setup({
